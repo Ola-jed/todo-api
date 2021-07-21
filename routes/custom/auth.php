362d77a7b84b61126c0e4fd55016d03a1ff82ab2
olabijed@gmail.com <olabijed@gmail.com>
2021-07-21 10:33:09 +0100

    Password reset route

--- routes/custom/auth.php
+++ routes/custom/auth.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/signup',[UserAuthController::class,'signup']);
 Route::post('/signin',[UserAuthController::class,'signin'])
     ->name('login');
+Route::post('/password-reset',[UserAuthController::class,'signin']);
 Route::post('/logout',[UserAuthController::class,'logout'])
     ->middleware('auth:sanctum');
